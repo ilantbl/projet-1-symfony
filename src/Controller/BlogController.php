275c0c8e2352df617e21ee5b9398cac9fcdf8ed0
Ilan <ilan.teboul7@hotmail.fr>
2021-11-15 21:19:38 +0100

    init-1

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,17 +5,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 
 class BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="blog")
      */
-    public function index(): Response
+    public function index(ArticleRepository $repo): Response
     {
+        $articles = $repo->findAll();
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            'articles' => $articles
         ]);
+
     }
 
     /**
@@ -29,10 +37,32 @@ class BlogController extends AbstractController
             'age' => 17
         ]);
     }
+
     /**
-     * @Route ("/blog /12" , name="blog_show")
+     * @Route ("/blog/new", name="blog_create")
      */
-    public function show() {
-        return $this-> render('blog/show.html.twig');
+    public function create(Request $request, ObjectManager $manager) {
+
+        $article = new Article();
+        $form = $this->createFormBuilder($article)
+                     ->add('title')
+                     ->add('content')
+                     ->add('content')
+                     ->getForm();
+
+        return $this-> render('blog/create.html.twig', [
+            'formArticle' => $form->createView()
+        ]);
     }
+
+    /**
+     * @Route ("/blog/{id}" , name="blog_show" )
+     */
+    public function show(Article $article) {
+
+        return $this-> render('blog/show.html.twig', [
+            'article' => $article
+        ]);
+    }
+    
 }
